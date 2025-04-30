04325d0fd9af2eeed4e49bb050248ebc8d1cce0d
HR <153944081+CactiBestPlant@users.noreply.github.com>
2025-04-30 15:27:00 -0700

    Camera moves(woo)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,60 +1,19 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject playerObject; // Reference to the player GameObject
-    public Vector3 offset;          // Offset between the camera and the player
-    private Transform player;       // Cached reference to the player's transform
-
+    Vector3 offset;
+    GameObject Player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (playerObject != null)
-        {
-            // Cache the player's transform
-            player = playerObject.transform;
-        }
+        Player = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - Player.transform.position;
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        if (player != null)
-        {
-            transform.position = player.position + offset;
-        }
-    }
-    
-    void LateUpdate()
-    {
-        if (player != null)
-        {
-            // Update the camera's position to follow the player with the offset
-            transform.position = player.position + offset;
-        }
-        else
-        {
-            Debug.LogWarning("Problem 4");
-        }
-    }
-
-    void OnEnable()
-    {
-        // Subscribe to the sceneLoaded event
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnDisable()
-    {
-        // Unsubscribe from the sceneLoaded event to avoid memory leaks
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (playerObject != null)
-        {
-            // Cache the player's transform after the scene has loaded
-            player = playerObject.transform;
-        }
+        transform.position = Player.transform.position + offset;
     }
 }
