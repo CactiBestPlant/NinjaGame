4e6a70efedf591a0d61c4db7d674cedbfc082768
HR <153944081+CactiBestPlant@users.noreply.github.com>
2025-05-22 15:16:43 -0700

    i am tired an now apathetic

--- Assets/Scripts/RapierMoveScript.cs
+++ Assets/Scripts/RapierMoveScript.cs
@@ -2,19 +2,35 @@ using UnityEngine;
 
 public class RapierMoveScript : MonoBehaviour
 {
-    Vector3 rapiermove;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public Transform target;
+    private Vector3 offset;
+    RapierScript rapierScript;// Reference to the RapierScript instance
+
     void Awake()
     {
+        if (target != null)
+        {
+            offset = transform.position - target.position+ new Vector3(1, 0, 0);
+        }
 
-     rapiermove = new Vector3(1, 0, 0);
+        // Find the RapierScript component on the same GameObject or another GameObject
+        rapierScript = FindObjectOfType<RapierScript>();
+        if (rapierScript == null)
+        {
+            Debug.LogError("RapierScript instance not found!");
+        }
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //     rapiermove += RapierScript.playerpos;
-
-
-    //}
+    void Update()
+    {
+        if (rapierScript.checkrapout == 0)
+        {
+            Destroy(gameObject);
+        }
+        if (rapierScript != null)
+        {
+            transform.position = Vector3.Lerp( transform.position, target.position + offset, Time.deltaTime * 15f);
+        }
+    }
 }
--- Assets/Scripts/RapierScript.cs
+++ Assets/Scripts/RapierScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RapierScript : MonoBehaviour
 {
+    public float checkrapout;
+    float rapinst;
     public GameObject rapier;
     float lungeDist;
     float lungeactive;
@@ -10,12 +12,14 @@ public class RapierScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        lungeactive=0; 
+        checkrapout = 0;
+        lungeactive =0; 
     }
 
     // Update is called once per frame
     void Update()
     {
+       int rapinst = GameObject.FindGameObjectsWithTag("weapon").Length; // Correct method
         playerpos = transform.position;
         Vector3 movement = new Vector3(lungeDist, 0, 0);
        if(Input.GetMouseButton(0))
@@ -36,25 +40,37 @@ public class RapierScript : MonoBehaviour
 
        if(lungeactive==1)
        {
-        //transform.Translate(movement*5*Time.deltaTime);
-        //lungeDist-=1*Time.deltaTime; 
-        //Quaternion spawnRotation = Quaternion.identity;
-        //instantiate (rapier, playerpos+1, spawnRotation);
-        if(lungeDist<=0)
-        {
-            lungeactive=0;
-        }
+        transform.Translate(movement*5*Time.deltaTime);
+        lungeDist-=1*Time.deltaTime;
+        if (lungeDist <= 0)
+            {
+                    lungeactive = 0;
+            }
        }
        else
        {
         lungeDist=0;
        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            checkrapout = 1;
+            if (rapinst < 1)
+            {
 
-       if(Input.GetKeyDown(KeyCode.E) && lungeDist<1.5)
-       {
-        print("Check Stop");
-        lungeactive=0;
-       }
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(rapier, playerpos, spawnRotation);
+            }
+        }
+        else
+        {
+            checkrapout = 0;
+        }
+
+       if (Input.GetKeyDown(KeyCode.E) && lungeDist < 1.5)
+        {
+            print("Check Stop");
+            lungeactive = 0;
+        }
 
     }
 
